65b6269fa6868a010fcf9972e750c021dcdfd1ef
Nastya Shkrob <shkrobnastya@gmail.com>
2025-11-16 17:23:47 +0100

    refactor index file

--- index.ts
+++ index.ts
@@ -1,47 +1,51 @@
-import { WebSocketServer, RawData } from 'ws';
-import { httpServer } from "./src/http_server/index";
-import { handleRequests } from "./src/handlers/messageHandler";
-import {sendJSON} from "./src/utils/helpers";
+import { WebSocketServer, WebSocket } from 'ws';
+import { httpServer } from './src/http_server/index';
+import { handleRequests } from './src/handlers/messageHandler';
+import { sendJSON } from './src/utils/helpers';
 
-function onSocketError(err: any) {
-  console.error(err);
-}
 const wss = new WebSocketServer({ port: 3000 });
 const HTTP_PORT = 8181;
 
-wss.on('connection', function connection(ws: any, request:any, client:any) {
+wss.on('connection', function connection(ws: WebSocket) {
   console.log('New client connected');
 
-  ws.on('error', console.error);
+  ws.on('error', handleSocketError);
 
-
-  ws.on('message', (msg:string) => {
+  ws.on('message', (msg: string) => {
     try {
-      console.log(msg.toString());
       handleRequests(ws, msg);
     } catch (e) {
       console.error('Invalid JSON from client', e);
-      sendJSON(ws, JSON.stringify({
-        type: 'error',
-        data: { message: 'Invalid JSON' },
-        id: 0,
-      }));
+      sendJSON(
+        ws,
+        JSON.stringify({
+          type: 'error',
+          data: { message: 'Invalid JSON' },
+          id: 0,
+        }),
+      );
     }
   });
 
   ws.on('close', () => console.log('Client disconnected'));
-
 });
+
+httpServer.on('upgrade', (req, socket) => {
+  socket.on('error', handleSocketError);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Static HTTP server running on port ${HTTP_PORT}`);
+});
+
+function handleSocketError(err: unknown) {
+  console.error(err);
+}
 
 process.on('SIGINT', () => {
-  console.log('🛑 Server shutting down...');
-  wss.close();
-  process.exit(0);
+  console.log('🛑 Shutting down...');
+  wss.close(() => {
+    console.log('WebSocket server closed');
+    process.exit(0);
+  });
 });
-
-httpServer.on('upgrade', function upgrade(request, socket, head) {
-  socket.on('error', onSocketError);
-});
-
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
